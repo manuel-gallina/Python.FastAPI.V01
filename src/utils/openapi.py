96adc7ee223eddda19e4da8518615840563c459c
Manuel Gallina <manuel.gallina@3ds.team>
2026-02-11 16:52:15 +0100

    feat: Update export_schema function to accept Path type and improve file writing

--- src/utils/openapi.py
+++ src/utils/openapi.py
@@ -1,19 +1,22 @@
+from pathlib import Path
+
 import yaml
 
 from main import app
 
 
-def export_schema(dst: str) -> None:
+def export_schema(dst: str | Path) -> None:
     """
     Export the OpenAPI schema of the FastAPI application to a YAML file.
 
     Args:
-        dst (str): The destination file path where the OpenAPI schema will be saved.
-
-    Returns:
-
+        dst (str | Path): The destination file path where the OpenAPI schema will be saved.
     """
     openapi_schema = app.openapi()
     openapi_schema["info"]["x-logo"] = {"url": "./logo/logo.png", "altText": "Logo"}
-    with open(dst, "w") as f:
-        yaml.dump(openapi_schema, f, sort_keys=False)
+    openapi_schema_file = Path(dst)
+    openapi_schema_file.write_text(
+        yaml.dump(openapi_schema, None, sort_keys=False),
+        encoding="utf-8",
+        newline="\r\n",
+    )
